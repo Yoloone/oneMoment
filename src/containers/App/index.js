adde55b4887e335aed8af1f33520484d99caab19
Yoloone <2858010791@qq.com>
2019-08-26 22:25:41 +0800

    cors solve

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Column from '../Column';
 import TodayArticle from '../Article/todayArticles';
 import HistoryArticle from '../Article/historyArticles';
